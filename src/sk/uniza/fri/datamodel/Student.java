549abf72bbca8a73871862e89b8ce264d7bcf529
absolutty <ada.hajro@gmail.com>
2022-11-03 15:30:34 +0100

    Reading from CSV and storing data to a object.

--- src/sk/uniza/fri/datamodel/Student.java
+++ src/sk/uniza/fri/datamodel/Student.java
@@ -1,31 +1,21 @@
 package sk.uniza.fri.datamodel;
 
-import java.util.ArrayList;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
 
+@XStreamAlias("student")
 public class Student {
 
     private final String id;
     private final String name;
     private final String surname;
-    private ArrayList<Study> study;
 
     public Student(String id, String name, String surname) {
         this.id = id;
         this.name = name;
         this.surname = surname;
-        this.study = new ArrayList<>();
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
     }
 
 }
+
+
+
